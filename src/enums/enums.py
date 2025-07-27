06823d8d0762d1efb7b6ca96808bbe9feac3f423
inickname <nikolaysmorodinov1@gmail.com>
2025-07-27 18:56:44 +0300

    Bug Fixes

--- src/enums/enums.py
+++ src/enums/enums.py
@@ -5,13 +5,5 @@ class Url(Enum):
     BASE_URL = "https://restful-booker.herokuapp.com"
 
 
-def return_base_url(url: Url) -> str:
-    return url.value
-
-
 class Headers(Enum):
     HEADERS = {"Content-Type": "application/json", "Accept": "application/json"}
-
-
-def return_headers(api_headers: Headers) -> dict:
-    return api_headers.value
