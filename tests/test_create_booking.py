27ec56f831cac788cfa66cdbbe7cad1de9df2fe2
inickname <nikolaysmorodinov1@gmail.com>
2025-07-27 21:14:09 +0300

    Added allure

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -1,29 +1,36 @@
+import allure
+
 from src.api.booking_api_client import BookingApiClient
 from src.scenarios.booking_scenarios import BookingScenarios
 from tests.conftest import auth_session
 
 
 class TestBookings:
+    @allure.title("Получение списка bookings")
     def test_get_and_verify_bookings_exist(self, auth_session):
         booking_api_client = BookingApiClient(auth_session)
         booking_scenarios = BookingScenarios(booking_api_client)
         booking_scenarios.get_and_verify_bookings_exist()
 
+    @allure.title("Cоздание booking")
     def test_create_booking_check_and_delete(self, auth_session, booking_data):
         booking_api_client = BookingApiClient(auth_session)
         booking_scenarios = BookingScenarios(booking_api_client)
         booking_scenarios.create_booking_check_and_delete(booking_data)
 
+    @allure.title("Изменение booking")
     def test_update_booking_and_verify_changes(self, auth_session, booking_data):
         booking_api_client = BookingApiClient(auth_session)
         booking_scenarios = BookingScenarios(booking_api_client)
         booking_scenarios.update_booking_and_verify_changes(booking_data)
 
+    @allure.title("Частичное изменение booking")
     def test_partial_update_booking_and_verify_changes(self, auth_session, booking_data):
         booking_api_client = BookingApiClient(auth_session)
         booking_scenarios = BookingScenarios(booking_api_client)
         booking_scenarios.partial_update_booking_and_verify_changes(booking_data)
 
+    @allure.title("Удаление booking")
     def test_delete_existing_booking_and_verify(self, auth_session, booking_data):
         booking_api_client = BookingApiClient(auth_session)
         booking_scenarios = BookingScenarios(booking_api_client)
