c314e84d4972d8bdbf15feff85c09554a6b6da50
inickname <nikolaysmorodinov1@gmail.com>
2025-08-24 19:03:47 +0300

    Added allure.step

--- src/api/booking_api_client.py
+++ src/api/booking_api_client.py
@@ -1,3 +1,5 @@
+import allure
+
 from src.enums.enums import Url
 
 
@@ -6,6 +8,7 @@ class BookingApiClient:
         self.auth_session = auth_session
         self.base_url = Url.BASE_URL.value
 
+    @allure.step("Создание booking")
     def create_booking(self, booking_data):
         """Отправляет запрос на создание booking."""
         response = self.auth_session.post(f"{self.base_url}/booking", json=booking_data.model_dump())
@@ -14,6 +17,7 @@ class BookingApiClient:
             response.raise_for_status()  # Выбросит HTTPError для плохих статусов
         return response
 
+    @allure.step("Получение bookings")
     def get_bookings(self, booking_id=""):
         """Отправляет запрос на получение списка bookings."""
         response = self.auth_session.get(f"{self.base_url}/booking/{booking_id}")
@@ -21,6 +25,7 @@ class BookingApiClient:
             response.raise_for_status()
         return response
 
+    @allure.step("Обновление booking")
     def update_booking(self, booking_id, booking_data):
         """Отправляет запрос на обновление booking."""
         response = self.auth_session.put(f"{self.base_url}/booking/{booking_id}", json=booking_data.model_dump())
@@ -28,12 +33,15 @@ class BookingApiClient:
             response.raise_for_status()
         return response
 
+    @allure.step("Частичное обновление booking")
     def partial_update_booking(self, booking_id, booking_data):
+        """Отправляет запрос на частичное обновление booking."""
         response = self.auth_session.patch(f"{self.base_url}/booking/{booking_id}", json=booking_data.model_dump())
         if response.status_code != 200:
             response.raise_for_status()
         return response
 
+    @allure.step("Удаление booking")
     def delete_booking(self, booking_id):
         """Отправляет запрос на удаление booking."""
         response = self.auth_session.delete(f"{self.base_url}/booking/{booking_id}")
